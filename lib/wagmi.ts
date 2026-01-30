7d05453e9462c1dedd0bc7bb9c70de9adc2905f0
ditsy <Ardiansyah.ditsy@gmail.com>
2026-01-30 22:12:49 +0700

    main

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,11 +1,11 @@
 import { createConfig, http } from "wagmi";
 import { base } from "wagmi/chains";
-import { farcasterConnector } from "@farcaster/miniapp-wagmi-connector";
+import { farcasterFrame } from "@farcaster/miniapp-wagmi-connector";
 
 export const config = createConfig({
   chains: [base],
   connectors: [
-    farcasterConnector(),
+    farcasterFrame(),
   ],
   transports: {
     [base.id]: http(),
